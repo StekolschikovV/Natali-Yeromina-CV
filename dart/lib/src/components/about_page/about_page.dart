893a0c848cfe728f624b8b3e295d1b3671514f88
v.a.stekolschikov <v.a.stekolschikov@gmail.com>
2018-11-16 13:19:14 +0200

    dart: temp

---
+++ dart/lib/src/components/about_page/about_page.dart
@@ -0,0 +1,15 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'about-page',
+  styleUrls: ['about_page.css'],
+  templateUrl: 'about_page.html',
+)
+class AboutPage  {
+
+  IndexPage(){
+    print('AboutPage ok');
+  }
+
+
+}
