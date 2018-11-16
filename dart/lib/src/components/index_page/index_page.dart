071e67d5d0e5647ff3ae4d931899bef3fbae67a3
v.a.stekolschikov <v.a.stekolschikov@gmail.com>
2018-11-16 12:40:22 +0200

    dart: add nav position

---
+++ dart/lib/src/components/index_page/index_page.dart
@@ -0,0 +1,15 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'index-page',
+  styleUrls: ['index_page.css'],
+  templateUrl: 'index_page.html',
+)
+class IndexPage  {
+
+  IndexPage(){
+    print('IndexPage ok');
+  }
+
+
+}
